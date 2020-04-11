3a7d8a7a80ba0bdd7382a64a5a073520043f0a6b
Saullo Almeida <saullorhamon@gmail.com>
2020-04-10 23:02:31 -0300

    Implementação de Login e Logout
    *Navegação entre telas de login e logout
    *main->wrapper->home ou authenticate
    *auth.dart tras toda a responsabilidade do login
    *user.dart para facilitar pegar apenas os dados necessarios que o firebase disponibilisa.

---
+++ lib/services/auth.dart
@@ -0,0 +1,36 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  
+  User _userFromFirebaseUser(FirebaseUser user){
+    return user != null ? User(uid: user.uid) : null;
+  } 
+
+  Stream<User> get user{
+    return _auth.onAuthStateChanged
+    //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+    .map(_userFromFirebaseUser);
+  }
+
+  Future signInAnon() async{
+    try{
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut()async{
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
