0012374a24f0490767415c40fc079aeefbdcce54
Saullo Almeida <saullorhamon@gmail.com>
2020-04-11 11:44:11 -0300

    add ToggleView
    *alternancia entre os formularios de entrar e registrar

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,81 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  final Function toggleView;
+
+  const Register({Key key, this.toggleView}) : super(key: key);
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+  
+  String email = '';
+  String password = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text("Sign up to Brew Crew"),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: () {
+              widget.toggleView();
+            },
+            icon: Icon(Icons.person),
+            label: Text("Sign In"),
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 20.0,
+          horizontal: 50.0,
+        ),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Email"),
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Password"),
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              RaisedButton(
+                onPressed: () async {
+                  print(email);
+                  print(password);
+                },
+                color: Colors.pink[400],
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,12 +2,18 @@ import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
+  final Function toggleView;
+
+  const SignIn({Key key, this.toggleView}) : super(key: key);
+
   @override
   _SignInState createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  String email = '';
+  String password = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +22,15 @@ class _SignInState extends State<SignIn> {
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
         title: Text("Sign in to Brew Crew"),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: () {
+              widget.toggleView();
+            },
+            icon: Icon(Icons.person),
+            label: Text("Register"),
+          )
+        ],
       ),
       body: Container(
           padding: EdgeInsets.symmetric(
@@ -25,17 +40,35 @@ class _SignInState extends State<SignIn> {
           child: Form(
             child: Column(
               children: <Widget>[
-                SizedBox(height: 20.0,),
+                SizedBox(
+                  height: 20.0,
+                ),
                 TextFormField(
-                  onChanged: (val){
-                    
+                  decoration: InputDecoration(hintText: "Email"),
+                  onChanged: (val) {
+                    setState(() => email = val);
                   },
                 ),
                 TextFormField(
+                  decoration: InputDecoration(hintText: "Password"),
                   obscureText: true,
-                  onChanged: (val){
-
+                  onChanged: (val) {
+                    setState(() => password = val);
                   },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RaisedButton(
+                  onPressed: () async {
+                    print(email);
+                    print(password);
+                  },
+                  color: Colors.pink[400],
+                  child: Text(
+                    "Sign in",
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ],
             ),
